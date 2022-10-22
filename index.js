a5962954eb4bc54e8d9cff7fbebd8326f9fa6b23
Darx <mcrivabode@gmail.com>
2022-10-21 20:45:01 -0700

    add more questions

--- index.js
+++ index.js
@@ -22,11 +22,53 @@ const questions = [
         type: "input",
         name: "description",
         message: "Please provide a description for your project.",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log("You must enter a valid description.");
+                return false;
+            }
+        }
     },
     {
         type: "input",
-        name: "description",
-        message: "Please provide a description for your project.",
+        name: "installation",
+        message: "What must be done to install your project?",
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log("Please provide installation details.");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is the usage of this project?",
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log("Please describe how this project is used.");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "How can other users contribute to this project?",
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log("Please describe how users may contribute to your project.");
+                return false;
+            }
+        }
     },
 ];
 
