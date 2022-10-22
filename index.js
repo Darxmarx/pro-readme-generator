fc9c4e25236d459f8614c498c4d048a3398207b0
Darx <mcrivabode@gmail.com>
2022-10-21 21:02:51 -0700

    add function to write readme in index.js

--- index.js
+++ index.js
@@ -118,7 +118,14 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("Your README was successfully generated.")
+    });
+};
 
 // TODO: Create a function to initialize app
 function init() {}
