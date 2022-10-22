f042cca36237e9dfb079de9aba8dfe63f4eee4da
Darx <mcrivabode@gmail.com>
2022-10-21 20:32:41 -0700

    add required packages

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const utils = require("utils") //utils folder
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
