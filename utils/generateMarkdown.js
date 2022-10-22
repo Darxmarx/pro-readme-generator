1b310dcac55db8d03b629b9a1f68e5705f83f54b
Darx <mcrivabode@gmail.com>
2022-10-21 21:19:02 -0700

    add badges based on license selected

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,15 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = "";
+  if (license === "Apache") {
+    badge = "![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+  }
+  if (license === "MIT") {
+    badge = "![GitHub license](https://img.shields.io/badge/License-MIT-green.svg)";
+  }
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
